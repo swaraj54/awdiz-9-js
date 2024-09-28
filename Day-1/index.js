784fa9405f8814eab4faf35a304ecc9cdc88870e
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2024-09-28 14:56:49 +0530

    Updates

---
+++ Day-1/index.js
@@ -0,0 +1,109 @@
+1. Datatypes
+String
+Number
+Boolean
+undefined
+null
+2. Varible
+var
+let
+const
+
+
+
+
+1.1 String
+"" '' ``
+
+"awdiz"
+'vashi'
+`students`
+'1'
+"234"
+`3245#@$%^&#@#$`
+
+function -> multiple lines of code -> feature return
+
+console.log(typeof("awdiz"))
+console.log(typeof('awdiz'))
+console.log(typeof(`awdiz`))
+console.log(typeof("1"))
+console.log(typeof('2'))
+console.log(typeof(`3`))
+
+Number
+
+12
+2121
+4546
+console.log(typeof(a))
+console.log(typeof(2.23))
+console.log(typeof("@@#$$"))
+
+Boolean true false
+
+console.log(typeof('true'))
+console.log(typeof('false'))
+
+Assign =
+re assign  =
+declare =
+
+var - store data
+
+var myName = "awdiz";
+console.log(myName, "myName");
+
+var myage = 10;
+console.log(myage, "myage");
+
+var myage = '10';
+console.log(myage, "myage");
+
+var number = 10; // declare and assign
+console.log(number);
+number = 20; // re assign
+console.log(number);
+number = 30; // re assign
+console.log(number);
+number = 40; // re assign
+console.log(number);
+number = 50; // re assign
+console.log(number);
+var number = 60; // re-declare and assign
+console.log(number)
+
+var number1 = 20;
+var number2 = 40;
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+
+= -> to assign a value
+
+1. Comparision operator
+== -> comparision only data
+=== -> comparision data with datatypes
+>
+<
+<=
+>=
+
+console.log(1 < 2);
+console.log(1 > 2);
+var number1 = 1;
+var number2 = 1;
+console.log(number1 >= number2);
+
+var firstName = "hii";
+var secondName = "hii";
+console.log(first == second);
+
+console.log("11111" == 11111);
+console.log("11111" === "11111");
+
+// difference between var let and const
+//                 var
+// reassign        true
+// redeclare       true
