25201039ef06a5e9f83c903dacc32a67a446123b
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2024-10-08 21:10:10 +0530

    Updates

---
+++ Day-6/index.js
@@ -0,0 +1,139 @@
+// 1. Datatypes
+// String
+// Number
+// Boolean
+// undefined
+// null - pending
+// 2. Varible
+// var
+// let
+// const
+// Comparision operator , == , === , < , >
+// Conditional statements
+// 1. if else
+// 2. switch
+// Logical Operators
+// && and operator,  || or operator
+// inbuilt functions console.log() typeof()
+// we can create our own functions
+// function -> its provide functionality / usecase , write multiple lines of code
+// array
+// loops -> for , while
+
+// Print numbers from 1 - 1000
+
+// function printNumbers(){
+//     console.log(1)
+//     console.log(2)
+//     console.log(3)
+//     console.log(10)
+// }
+// printNumbers();
+
+// for loop
+
+// for(starting ; ending ; sequence){
+//     multiple lines of code
+// }
+
+// for( 1 ; 10 ; 1){
+//    console.log()
+// }
+
+// if (true) {
+// }
+
+
+// 1 Iteration : 
+// var myNumber = 1;
+// myNumber < 11
+// 1 < 11 => true 
+// log(myNumber) => 1
+// myNumber++
+// myNumber = 2
+
+// 2nd Iteration :
+// myNumber = 2
+// myNumber < 11
+// 2 < 11 => true
+// log(myNumber) => 2
+// myNumber++
+// myNumber = 3
+
+
+// 3rd iteration 
+// myNumber = 3
+// myNumber < 11
+// 3 < 11 => true 
+// log(myNumber);  3
+// myNumber++
+// myNumber = 4
+
+
+// 4 th iteration 
+// myNumber = 4;
+// myNumber < 11
+// 4 < 11 => true 
+// log(myNumber) // 4
+// myNumber++; 
+// myNumber = 5
+
+
+for (var myNumber = 1; myNumber < 11; myNumber++) {
+  console.log(myNumber); // 1
+//   myNumber++
+}
+
+// 10 th iteration 
+// myNumber = 9;
+// myNumber < 11;
+// 9 < 11 true 
+// log(myNumber) 9
+// myNumber++
+// myNumber = 10;
+
+// 11 th iteration 
+// myNumber = 10;
+// myNumber < 11
+// 10 < 11 true 
+// log(myNumber) 10 
+// myNumber++
+// myNumber = 11;
+
+// 12 iteration 
+
+// myNumber = 11;
+// myNumber < 11;
+// 11 < 11 => false 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num = 17;
+// num--; // 16
+// num += 2; // 18
+// num++; // 19
+// num = num - 1; // 18
+// num -= 1; // 17
+// num = num + 4;
+// num = num - 2;
+// num++; // increment operator -  it'll increment value by 1
+// num++; // it'll increment value by 1
+// num--;
+// num--;
+// num += 3;
+// num -= 3;
+
+// console.log(num);
